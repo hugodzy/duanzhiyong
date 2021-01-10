fa97bacb56724981583d9c17080e59506fe1d156
hugo <347501437@qq.com>
2021-01-10 21:28:05 +0800

    s

---
+++ src/main/java/com/itheima/phoneix/test.java
@@ -0,0 +1,13 @@
+package com.itheima.phoneix;
+
+/**
+ * description:
+ *
+ * @auther hugo
+ * @date 2021/1/10 21:15
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
